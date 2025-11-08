40b014ae628922586c23c5f5a6fb29ff90bb1346
Stiven Garzon <usg210802@outlook.com>
2025-11-08 16:14:25 -0500

    modificacion de cors

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:[
+    "http://localhost:5173",
+    "https://autenticacion-firebase.onrender.com"
+  ],
+
   methods:['POST','GET'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }))
